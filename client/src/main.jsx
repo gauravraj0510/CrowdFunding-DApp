81788ac474dccfa147e862df9f12728343077ea6
gauravraj0510 <gaurav.rajghs@gmail.com>
2023-06-19 13:16:31 +0530

    client setup completed

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider activeChain={Sepolia}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
